ec4c2d742d47cdeb91a3d50e6413e8fc1be89cf8
Teodoro Obando <teo.obando09@gmail.com>
2022-09-06 15:38:31 -0600

    [ADD] campo edad
    Validacion que tome solo edad mayor de 20

--- src/components/CompForm.tsx
+++ src/components/CompForm.tsx
@@ -1,49 +1,40 @@
 import React from 'react'
 import { useState } from 'react'
 
-function CompForm() {
+const CompForm = () => {
 
     const [name, setName] = useState('')
     const [lastName, setlastName] = useState('')
+    const [age, setAge] = useState(20) 
 
     const handleChange = (e) => {
         console.log(e)
         if (e.target.name === 'nombre') {
             setName(e.target.value)
-        } else {
+        } else if(e.target.name === 'apellido') {
             setlastName(e.target.value)
+        } else {
+            setAge(e.target.value < 20 ? 20 : e.target.value)
         }
     }
 
     return (
-        <div
-            style={{
-                padding: '100px',
-                textAlign: 'center',
-            }}
-        >
-
-            <label>Nombre</label>
+        <div style={{ padding: '100px', textAlign: 'center' }}>
+            <label>Nombre </label>
             <input 
-                type="text" 
-                defaultValue={name}
-                name="nombre"
-                id="nombre"
-                onChange={handleChange}
+                type="text" defaultValue={name} name="nombre" onChange={handleChange}
             />
-            <hr />
-            <label>Apellido</label>
+            <label> Apellido</label>
             <input 
-                type="text"
-                defaultValue={lastName}
-                name="apellido"
-                id="apellido"
-                onChange={handleChange} 
+                type="text" defaultValue={lastName} name="apellido" onChange={handleChange} 
             />
-            <p>{ name } { lastName }</p>
-
+            <label> Edad</label>
+            <input 
+                type="number" defaultValue={age} onChange={handleChange}
+            />
+            <p>{ name } { lastName } {age}</p>
         </div>
     )
 }
 
-export default CompForm
+export default CompForm;
